8151dca016f4659f0e17e8cf76655ce0903bcd4b
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2015-05-29 20:53:13 +0300

    remove garbage

--- colcalc.py
+++ colcalc.py
@@ -18,47 +18,6 @@ import os
 import math
 
 
-def expected_value(f, col):
-    M = 0.
-    I = 0.
-    D = 0.
-    start = True
-    for line in f:
-        if line[0] == '#':
-            continue
-        if start:
-            spl = line.split()
-            xp = float(spl[0])
-            yp = float(spl[col])
-            start = False
-        else:
-            spl = line.split()
-            x = float(spl[0])
-            y = float(spl[col])
-            M += (y * x + yp * xp) * (x - xp) / 2.
-            D += (y * x ** 2 + yp * xp ** 2) * (x - xp) / 2.
-            I += (y + yp) * (x - xp) / 2.
-            xp = x
-            yp = y
-    print("%g\t%g" % (M / I, D / I - (M / I) ** 2))
-
-
-def med_ariph(f, col):
-    N = 0
-    M = 0.
-    M2 = 0.
-    for line in f:
-        if line[0] != '#':
-            i = float(line.split()[col])
-            N += 1
-            M += i
-            M2 += i * i
-    M_ = M / N
-    S2_ = M2 / N - M_ ** 2
-    S_ = math.sqrt(S2_)
-    print(M_, S2_, S_, N, '|', M, M2)
-
-
 def lin_stat(idata, col_x, col_y):
     ipoints = []
     for data in idata:
