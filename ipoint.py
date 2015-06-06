66242f69f2835c8ef3956cb08364674ea5d094f2
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2015-06-06 15:29:46 +0300

    implement --mode option

--- ipoint.py
+++ ipoint.py
@@ -20,31 +20,24 @@ from read_data import InputData
 def parse_options():
     from argparse import ArgumentParser
     parser = ArgumentParser(description="Calculates experimental statistics.")
+    parser.add_argument(
+        "-m", "--mode", dest="mode", type=str, required=True, metavar="Mode",
+        choices={"aver", "average", "disp", "dispersion", "lr",
+                 "linear-regresion", "pol", "polinomial", "caver",
+                 "column-average"}, help="Calculation mode")
     parser.add_argument("--version", action="version", version="%(prog)s 0.1")
     parser.add_argument(
         "-o", "--out", dest="ofilename",
         help="write result to to FILE", metavar="FILE", default=None)
     parser.add_argument(
-        "-a", "--average", dest="average",
-        action="store_true", default=False, help="calculate average")
-    parser.add_argument(
-        "-d", "--dispersion", dest="dispersion",
-        action="store_true", default=False, help="calculate dispersion")
+        "-d", "--degree", dest="degree", type=int, default=2,
+        help="degree of polinomial")
     parser.add_argument(
         "-c", "--column-y", type=int, help="column with y values", default=2,
         dest="col_y")
     parser.add_argument(
         "--column-x", type=int, help="column with x values", default=1,
         dest="col_x")
-    parser.add_argument(
-        "--lin-reg", action="store_true", default=False,
-        dest="lin_regr", help="linear regresion")
-    parser.add_argument(
-        "--polinomial", default=None, dest="polinomial",
-        help="Polinomial fit", type=int, metavar="degree")
-    parser.add_argument(
-        "--caver", action="store_true", default=False,
-        dest="col_aver", help="column average")
     parser.add_argument("files", metavar="File", type=str, nargs="+",
                         help="Files to process")
     return parser.parse_args()
@@ -54,19 +47,19 @@ def run():
     args = parse_options()
     idata = InputData(args.files)
     result = None
-    if args.average:
+    if args.mode in {"aver", "average"}:
         from calc1d import calc_med_ariph
         result = calc_med_ariph(idata)
-    if args.dispersion:
+    elif args.mode in {"disp", "dispersion"}:
         from calc1d import calc_dispersion
         result = calc_dispersion(idata, args.col_x - 1)
-    if args.lin_regr:
+    elif args.mode in {"lr", "linear-regresion"}:
         from colcalc import lin_stat
         result = lin_stat(idata, args.col_x - 1, args.col_y - 1)
-    if args.polinomial:
+    elif args.mode in {"pol", "polinomial"}:
         from colcalc import poly_fit
         result = poly_fit(
-            idata, args.col_x - 1, args.col_y - 1, args.polinomial)
+            idata, args.col_x - 1, args.col_y - 1, args.degree)
     if args.ofilename:
         ouf = open(args.ofilename, "w")
     else:
